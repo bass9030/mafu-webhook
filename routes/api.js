fc45be694b46f36d516baff4ee52929db00fef79
bass9030 <bass90301.naver.comm@gmail.com>
2024-01-19 22:00:49 +0900

    백엔드 코드 구현중

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,24 @@ var router = express.Router();
 const webhookManager = require('../utils/webhookManager');
 
 router.post('/register', function(req, res, next) {
-    let data = req.data;
-    webhookManager.addWebhook(data.url)
+    let data = req.body;
+    console.log(data);
+    try {
+        webhookManager.addWebhook(data.url)
+        res.json({status: 0})
+    }catch(e) {
+        res.json({status: -1})
+    }
 });
 
 router.post('/unregister', (req, res, next) => {
-    let data = req.data;
-    webhookManager.removeWebhook(data.url)
+    let data = req.body;
+    try {
+        webhookManager.removeWebhook(data.url)
+        res.json({status: 0})
+    }catch(e) {
+        res.json({status: -1})
+    }
 })
 
 module.exports = router;
