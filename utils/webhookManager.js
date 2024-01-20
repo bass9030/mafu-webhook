8b52594df3e3fc042802dca09080e6b72b392c20
bass9030 <bass90301.naver.comm@gmail.com>
2024-01-20 23:00:01 +0900

    웹훅 전송 수정

--- utils/webhookManager.js
+++ utils/webhookManager.js
@@ -12,23 +12,43 @@ class webhookManager {
     }
 
     static removeWebhook(url) {
-        db.prepare('DELETE FROM webhooks WHERE webhookURL = ?;').run(url);
+        let result = db.prepare('DELETE FROM webhooks WHERE webhookURL = ?;').run(url);
+        return {changes: result.changes}
+    }
+
+    static getWebhookCount() {
+        let count = db.prepare('SELECT COUNT(*) FROM webhookURL;').get();
+        console.log(count);
     }
 
     /**
      * @param {MessageBuilder} message 
      */
-    static async sendWebhook(message) {
+    static async sendWebhook(message, profileURL) {
         const webhooks = db.prepare('SELECT * FROM webhooks;').all()
         for(let i = 0; i < webhooks.length; i++) {
-            const hook = new Webhook(e.webhookURL);
-            await hook.send(message);
+            let e = webhooks[i];
+            try {
+                new Promise(async () => {
+                    const hook = new Webhook(e.webhookURL);
+                    hook.setAvatar(profileURL);
+                    hook.setUsername('마훅 - 마후 트윗 번역봇');
+                    await hook.send(message);
+                });
+            }catch(e){
+                console.error(`Failed to send webhook to ${e.webhookURL}`)
+                console.error(e)
+            }
         }
     }
 }
 
-process.on('SIGKILL', () => {
+setInterval(() => {
+    db.backup(`./backup/${new Date().getTime()}_webhooks.db`);
+}, 1000 * 60 * 60 * 2)
 
+process.on('exit', () => {
+    db.close();
 })
 
 module.exports = webhookManager;
