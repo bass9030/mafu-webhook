bab2e463bfca31647be8bbdce41f9d0d82bbe3aa
bass9030 <bass90301.naver.comm@gmail.com>
2024-05-27 00:30:11 +0900

    mysql로 전환중

--- utils/webhookManager.js
+++ utils/webhookManager.js
@@ -24,6 +24,8 @@ core.getConnection().then((db) => {
             'roleID VARCHAR(19),' +
             'sendNoticeMessage BOOLEAN' +
         ');');
+    }catch(e){
+        console.error(e);
     }finally{
         db.release();
     }
@@ -72,39 +74,36 @@ class webhookManager {
      * @param {MessageBuilder | string} message 
      */
     static async sendWebhook(message, isNoti) {
-        const webhooks = isNoti ? db.prepare('SELECT * FROM webhooks WHERE sendNoticeMessage = ?;').all(1) : db.prepare('SELECT * FROM webhooks;').all();
-        for(let i = 0; i < webhooks.length; i++) {
-            let e = webhooks[i];
-            try {
-                const hook = new Webhook(e.webhookURL);
-                // console.log(message.getJSON()['embeds'][0]['footer'])
-                if(typeof message == 'string')
-                    hook.setAvatar('https://mahook.bass9030.dev/logo.png');
-                else{
-                    let profileURL = message.getJSON()['embeds'][0]['footer']['icon_url'];
-                    if(!!profileURL) hook.setAvatar(profileURL);
-                } 
-                hook.setUsername('마훅 - 마후 트윗 번역봇');
-                if(!!e.roleID) {
-                    if(e.roleID == '@everyone' || e.roleID == '@here') await hook.send(e.roleID);
-                    else await hook.send('<@&' + e.roleID + '>');
+        try {
+            let db = await core.getConnection();
+            const webhooks = isNoti ? await db.query('SELECT * FROM webhooks WHERE sendNoticeMessage = ?;', [1]) : await db.query('SELECT * FROM webhooks;');
+             for(let i = 0; i < webhooks.length; i++) {
+                let e = webhooks[i];
+                try {
+                    const hook = new Webhook(e.webhookURL);
+                    // console.log(message.getJSON()['embeds'][0]['footer'])
+                    if(typeof message == 'string')
+                        hook.setAvatar('https://mahook.bass9030.dev/logo.png');
+                    else{
+                        let profileURL = message.getJSON()['embeds'][0]['footer']['icon_url'];
+                        if(!!profileURL) hook.setAvatar(profileURL);
+                    } 
+                    hook.setUsername('마훅 - 마후 트윗 번역봇');
+                    if(!!e.roleID) {
+                        if(e.roleID == '@everyone' || e.roleID == '@here') await hook.send(e.roleID);
+                        else await hook.send('<@&' + e.roleID + '>');
+                    }
+                    if(isNoti) await hook.info('마훅 공지사항', '', message);
+                    else await hook.send(message);
+                }catch(err){
+                    console.error(`Failed to send webhook to ${e.webhookURL}`)
+                    console.error(err)
                 }
-                if(isNoti) await hook.info('마훅 공지사항', '', message);
-                else await hook.send(message);
-            }catch(err){
-                console.error(`Failed to send webhook to ${e.webhookURL}`)
-                console.error(err)
-            }
+            }   
+        }finally{
+            db?.release()
         }
     }
 }
-
-setInterval(() => {
-    db.backup(`./backup/${new Date().getTime()}_webhooks.db`);
-}, 1000 * 60 * 60 * 12)
-
-process.on('exit', () => {
-    db.close();
-})
 
 module.exports = webhookManager;
