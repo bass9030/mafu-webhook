485534478ecb94898ecd5e72e28f6e763b03f781
bass9030 <bass90301.naver.comm@gmail.com>
2025-02-23 22:36:39 +0900

    자동 배포용 github action 수정

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 docker-compose -f docker-compose-dev.yaml down
-docker rmi -f $(docker images -aq "mahook*")
-docker build -t mahook-web -f dockerfile .
-docker build -t mahook-db -f dockerfile.mariadb .
+docker rmi $(docker images -aq "mahook-*-dev")
+docker build -t mahook-web-dev -f dockerfile .
+docker build -t mahook-db-dev -f dockerfile.mariadb .
 
 docker-compose -f docker-compose-dev.yaml up -d
 echo "docker build complete!"
