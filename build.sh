650a6035a7e818492719bc184cb56615545a768f
bass9030 <bass90301.naver.comm@gmail.com>
2025-06-06 14:58:43 +0000

    수정: 빌드스크립트 단순화

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-docker-compose -f docker-compose-dev.yaml down
-docker rmi $(docker images -aq "mahook-*-dev")
-docker build -t mahook-web-dev -f dockerfile .
-docker build -t mahook-db-dev -f dockerfile.mariadb .
-echo "docker build complete!"
-docker compose -f docker-compose-dev.yaml up
+# docker compose -f docker-compose-dev.yaml down
+# docker rmi $(docker images -aq "mahook-*-dev")
+# # docker build -t mahook-web-dev -f dockerfile .
+# # docker build -t mahook-db-dev -f dockerfile.mariadb .
+# # echo "docker build complete!"
+# docker compose -f docker-compose-dev.yaml build
+docker compose -f docker-compose-dev.yaml up --build --watch
